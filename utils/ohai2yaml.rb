8510a79ffa1c97ce5f3a61c133441c550576b7ed
Nicolas Szalay <nicolas.szalay@fotolia.com>
2011-01-14 15:30:48 +0100

    turned script into a chef report handler

--- utils/ohai2yaml.rb
+++ utils/ohai2yaml.rb
@@ -4,35 +4,45 @@
 #
 # nico <nico@rottenbytes.info>
 
-
 require 'ohai'
 
-def ohai_flatten(key, val, keys, result)
-    keys << key
-    if val.is_a?(Mash)
-        val.each_pair do |nkey, nval|
-            ohai_flatten(nkey, nval, keys, result)
+module Fotolia
+  class DumpOhai
 
-            keys.delete_at(keys.size - 1)
-        end
-    else
-        key = keys.join(".")
-        if val.is_a?(Array)
-            result[key] = val.join(", ")
+    def initialize(target)
+      @target = target
+    end
+
+    def ohai_flatten(key, val, keys, result)
+        keys << key
+        if val.is_a?(Mash)
+            val.each_pair do |nkey, nval|
+                ohai_flatten(nkey, nval, keys, result)
+
+                keys.delete_at(keys.size - 1)
+            end
         else
-            result[key] = val
+            key = keys.join(".")
+            if val.is_a?(Array)
+                result[key] = val.join(", ")
+            else
+                result[key] = val
+            end
         end
     end
+
+    def report
+      oh = Ohai::System.new
+      oh.all_plugins
+      facts = {}
+
+      oh.data.each_pair do |key, val|
+          ohai_flatten(key,val, [], facts)
+      end
+
+      fp=open(@target,"w")
+      fp.write(facts.to_yaml)
+      fp.close
+    end
+  end
 end
-
-oh = Ohai::System.new
-oh.all_plugins
-facts = {}
-
-oh.data.each_pair do |key, val|
-    ohai_flatten(key,val, [], facts)
-end
-
-fp=open(ARGV[0],"w")
-fp.write(facts.to_yaml)
-fp.close
