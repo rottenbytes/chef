7a926577bf7b56699b7e143ac576203cc6d8eb10
Nicolas Szalay <nicolas.szalay@fotolia.com>
2011-10-21 11:34:15 +0200

    now pushing diffs to queue

--- async_handler/async_handler.rb
+++ async_handler/async_handler.rb
@@ -1,40 +1,56 @@
 require "chef/handler"
 
 module Mycorp
-    class Summarize < Chef::Handler
-    attr_accessor :conf
-  
+  class Summarize < Chef::Handler
+    attr_accessor :conf, :conffile
+
     def initialize(args)
-      self.conf = YAML.load_file(args[:configfile])
+      self.conffile = args[:configfile]
     end
 
     def report
-    
+      self.conf = YAML.load_file(self.conffile)
       begin
         require "rubygems"
         require "stomp"
         require "base64"
-        
-        ignored_resources = [ "/var/cache/chef/handlers",
-                              "Chef::Handler::Mycorp::Summarize"
-                            ]
+
+        ignored_resources = [ "/etc/chef/ohai_plugins", "/var/cache/chef/handlers", "/var/cache/chef/backup/handlers" ]
         report_resources= []
         updated_resources.each do |r|
           if !ignored_resources.include?(r.name) then
             report_resources.push r.name
           end
         end
-        
+
+        diffs = {}
+        require 'find'
+        report_resources.each do |r|
+          # does it look like a diff-able file ?
+          if r.match(/.conf/) then
+            # find the latest backup
+            bkps = Dir.glob(Chef::Config[:file_backup_path]+r+".chef-*")
+            # found !
+            unless bkps.empty?
+              last_bkp = bkps[-1]
+              diff_cmd = "diff -Nru #{last_bkp} #{r}"
+              diffs[r] = %x(#{diff_cmd})
+            end
+          end
+        end
+
         summary = { :nodename => node.name.dup,
                     :elapsed_time => elapsed_time,
                     :start_time => start_time,
                     :end_time => end_time,
-                    :updated_resources => report_resources
+                    :updated_resources => report_resources,
+                    :diffs => diffs.to_a
                   }
-        
+
         cnx=Stomp::Client.new(self.conf[:stomp_user], self.conf[:stomp_password], self.conf[:stomp_server], self.conf[:stomp_port], true)
         msg=Base64.encode64(Marshal.dump(summary))
         cnx.publish(self.conf[:stomp_queue], msg, {:persistent => false})
+        cnx.close
       rescue Exception => e
         Chef::Log.error("Could not summarize and send back data for this run ! (#{e})")
       end
