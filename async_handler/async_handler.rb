ed83eab61459e4ffc6bdffb1e7c7601aacb4ed99
Nicolas Szalay <nicolas.szalay@fotolia.com>
2012-07-18 20:44:28 +0200

    fix diff handling, push back resource type too

--- async_handler/async_handler.rb
+++ async_handler/async_handler.rb
@@ -15,27 +15,27 @@ module Mycorp
         require "stomp"
         require "base64"
 
-        ignored_resources = [ "/etc/chef/ohai_plugins", "/var/cache/chef/handlers", "/var/cache/chef/backup/handlers" ]
-        report_resources= []
+        ignored_resources = [ "/etc/chef/ohai_plugins", "/var/cache/chef/handlers", "apt_get_update",
+                              "Chef::Handler::Fotolia::FailedRun", "Chef::Handler::Fotolia::Summarize",
+                              "/var/cache/chef/backup/handlers", "/usr/local/etc/chef/ohai_plugins"
+                            ]
+        report_resources = Hash.new
         updated_resources.each do |r|
           if !ignored_resources.include?(r.name) then
-            report_resources.push r.name
+            report_resources[r.name] = r.class.to_s.gsub("Chef::Resource::", "")
           end
         end
 
         diffs = {}
         require 'find'
-        report_resources.each do |r|
-          # does it look like a diff-able file ?
-          if r.match(/.conf/) then
-            # find the latest backup
-            bkps = Dir.glob(Chef::Config[:file_backup_path]+r+".chef-*")
-            # found !
-            unless bkps.empty?
-              last_bkp = bkps[-1]
-              diff_cmd = "diff -Nru #{last_bkp} #{r}"
-              diffs[r] = %x(#{diff_cmd})
-            end
+        report_resources.each do |res, type|
+          # find the latest backup
+          bkps = Dir.glob(Chef::Config[:file_backup_path]+res+".chef-*").sort
+          # found !
+          unless bkps.empty?
+            last_bkp = bkps[-1]
+            diff_cmd = "diff -Nru #{last_bkp} #{res}"
+            diffs[res] = %x(#{diff_cmd})
           end
         end
 
@@ -44,6 +44,7 @@ module Mycorp
                     :start_time => start_time,
                     :end_time => end_time,
                     :updated_resources => report_resources,
+                    :environment => node.chef_environment,
                     :diffs => diffs.to_a
                   }
 
@@ -57,4 +58,5 @@ module Mycorp
     end
 
   end # end of class Summarize
+
 end
